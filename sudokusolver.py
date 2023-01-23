698b291e8cf26b83d15761171e04f953170cfcdf
Sathwik <sathwik.y31@gmail.com>
2023-01-23 11:29:09 -0500

    Created a find_empty method to help find empty spaces in a given sudoku board.

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,3 +1,4 @@
+#Static Test Board
 board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
         [6, 0, 0, 1, 9, 5, 0, 0, 0],
         [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -8,6 +9,7 @@ board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
         [0, 0, 0, 4, 1, 9, 0, 0, 5],
         [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
+# Method to print the formatted sudoku board
 def print_board(board):
     for i in range(len(board)):
         if i > 0: print('')
@@ -15,3 +17,11 @@ def print_board(board):
         for j in range(len(board[i])):
             if j % 3 == 0 and j != 0: print(' | ', end = '')
             print(f' {board[i][j]} ', end = '')
+
+# Method to iterate through sodoku board and return
+# a tuple that the row and column of an empty space
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
