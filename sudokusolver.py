bbf28247c27c61f154c9d1c2806d55a683024a5e
Sathwik <sathwik.y31@gmail.com>
2023-01-23 12:24:28 -0500

    Implemented valid_position method and solve method to calculate the proper values and insert them.

--- sudokusolver.py
+++ sudokusolver.py
@@ -25,3 +25,48 @@ def find_empty(board):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j)
+    return None
+
+# Method to determine whether an input to a 
+# sudoku board space is valid
+def valid_position(board, num, pos):
+    # Checks Columns
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+    
+    #Checks Rows
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+    
+    #Check Sudoku Box
+    box_x, box_y = pos[1] // 3, pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+    
+    # After all conditions have been checked, True is returned.
+    return True
+
+# Method that implements a backtracking algorithm to solve the board
+def solve(board): 
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid_position(board, i, (row, col)):
+            board[row][col] = i
+            if solve(board): return True
+            board[row][col] = 0
+    return False
+
+print_board(board)
+print('\n' + '--' * 20)
+solve(board)
+print_board(board)
